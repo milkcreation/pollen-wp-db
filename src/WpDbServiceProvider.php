0ed53f398b2f94ee96aaea6241fee9c278c289aa
Jordy Manner <jordy.manner@milkcreation.fr>
2021-05-11 14:49:38 +0000

    wp-db >> 1.0.x-dev

--- src/WpDbServiceProvider.php
+++ src/WpDbServiceProvider.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Pollen\WpDb;
 
-use Pollen\Container\BaseServiceProvider;
+use Pollen\Container\BootableServiceProvider;
 
-class WpDbServiceProvider extends BaseServiceProvider
+class WpDbServiceProvider extends BootableServiceProvider
 {
     /**
      * @var string[]
